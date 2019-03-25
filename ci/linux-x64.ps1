150ab7774c956218456f84df8e423364cf817624
Ashley Mannix <ashleymannix@live.com.au>
2019-03-25 12:11:19 +1000

    do a CoreCLR publish in CI too

--- ci/linux-x64.ps1
+++ ci/linux-x64.ps1
@@ -4,4 +4,8 @@ cargo clean
 dotnet restore
 dotnet test
 
+# CoreCLR publish
+dotnet publish dotnet/Db.Api/Db.Api.csproj -c Release -f netcoreapp2.2
+
+# CoreRT publish
 dotnet publish dotnet/Db.Api/Db.Api.csproj -c Release -f netcoreapp2.2 -r linux-x64 /p:AotBuild=true
