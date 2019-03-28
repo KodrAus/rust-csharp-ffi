b8c155ceae5632c4854286b6154254af17bbf9b1
Ashley Mannix <ashleymannix@live.com.au>
2019-03-28 13:31:58 +1000

    clean dirs explicitly between publishes

--- ci/linux-x64.ps1
+++ ci/linux-x64.ps1
@@ -1,11 +1,17 @@
-dotnet clean
-cargo clean
+function Clean-OutputDirs {
+    Remove-Item -Recurse -Force dotnet/**/bin
+    Remove-Item -Recurse -Force dotnet/**/obj
 
-dotnet restore
+    Remove-Item -Recurse -Force target
+}
+
+Clean-OutputDirs
 dotnet test
 
 # CoreCLR publish
+Clean-OutputDirs
 dotnet publish dotnet/Db.Api/Db.Api.csproj -c Release -f netcoreapp2.2
 
 # CoreRT publish
+Clean-OutputDirs
 dotnet publish dotnet/Db.Api/Db.Api.csproj -c Release -f netcoreapp2.2 -r linux-x64 /p:AotBuild=true
