c2895736505e842c95a6be2115a8a7f8ca73b25f
Ashley Mannix <ashleymannix@live.com.au>
2019-04-15 13:21:34 +1000

    fix up AOT bin path

--- ci/linux-x64.ps1
+++ ci/linux-x64.ps1
@@ -10,17 +10,30 @@ if ($LastExitCode) { exit 1 }
 # CoreCLR publish
 Clean-OutputDirs
 
-dotnet publish dotnet/Db.Api/Db.Api.csproj -c Release -f netcoreapp2.2 -r linux-x64
+dotnet publish `
+    dotnet/Db.Api/Db.Api.csproj `
+    -c Release `
+    -f netcoreapp2.2 `
+    -r linux-x64
 if ($LastExitCode) { exit 1 }
 
-dotnet run -p dotnet/Db.Tests.Integration/Db.Tests.Integration.csproj -- dotnet/Db.Api/bin/Release/netcoreapp2.2/linux-x64/publish/Db.Api
+dotnet run `
+    -p dotnet/Db.Tests.Integration/Db.Tests.Integration.csproj `
+    -- dotnet/Db.Api/bin/Release/netcoreapp2.2/linux-x64/publish/Db.Api
 if ($LastExitCode) { exit 1 }
 
 # CoreRT publish
 Clean-OutputDirs
 
-dotnet publish dotnet/Db.Api/Db.Api.csproj -c Release -f netcoreapp2.2 -r linux-x64 /p:AotBuild=true
+dotnet publish `
+    dotnet/Db.Api/Db.Api.csproj `
+    -c Release `
+    -f netcoreapp2.2 `
+    -r linux-x64 `
+    /p:AotBuild=true
 if ($LastExitCode) { exit 1 }
 
-dotnet run -p dotnet/Db.Tests.Integration/Db.Tests.Integration.csproj -- dotnet/Db.Api/bin/Release/netcoreapp2.2/publish/linux-x64/Db.Api
+dotnet run `
+    -p dotnet/Db.Tests.Integration/Db.Tests.Integration.csproj `
+    -- dotnet/Db.Api/bin/Release/netcoreapp2.2/linux-x64/publish/Db.Api
 if ($LastExitCode) { exit 1 }
--- ci/win-x64.ps1
+++ ci/win-x64.ps1
@@ -10,17 +10,30 @@ if ($LastExitCode) { exit 1 }
 # CoreCLR publish
 Clean-OutputDirs
 
-dotnet publish dotnet/Db.Api/Db.Api.csproj -c Release -f netcoreapp2.2 -r win-x64
+dotnet publish `
+    dotnet/Db.Api/Db.Api.csproj `
+    -c Release `
+    -f netcoreapp2.2 `
+    -r win-x64
 if ($LastExitCode) { exit 1 }
 
-dotnet run -p dotnet/Db.Tests.Integration/Db.Tests.Integration.csproj -- dotnet/Db.Api/bin/Release/netcoreapp2.2/win-x64/publish/Db.Api.exe
+dotnet run `
+    -p dotnet/Db.Tests.Integration/Db.Tests.Integration.csproj `
+    -- dotnet/Db.Api/bin/Release/netcoreapp2.2/win-x64/publish/Db.Api.exe
 if ($LastExitCode) { exit 1 }
 
 # CoreRT publish
 Clean-OutputDirs
 
-dotnet publish dotnet/Db.Api/Db.Api.csproj -c Release -f netcoreapp2.2 -r win-x64 /p:AotBuild=true
+dotnet publish `
+    dotnet/Db.Api/Db.Api.csproj `
+    -c Release `
+    -f netcoreapp2.2 `
+    -r win-x64 `
+    /p:AotBuild=true
 if ($LastExitCode) { exit 1 }
 
-dotnet run -p dotnet/Db.Tests.Integration/Db.Tests.Integration.csproj -- dotnet/Db.Api/bin/Release/netcoreapp2.2/publish/win-x64/Db.Api.exe
+dotnet run `
+    -p dotnet/Db.Tests.Integration/Db.Tests.Integration.csproj `
+    -- dotnet/Db.Api/bin/Release/netcoreapp2.2/win-x64/publish/Db.Api.exe
 if ($LastExitCode) { exit 1 }
